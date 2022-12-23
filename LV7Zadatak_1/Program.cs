d20e49bd2193323b25b8e787ee9140394596dd06
Platypus712 <nikolapustisek@gmail.com>
2022-12-24 00:50:49 +0100

    LV7 added

---
+++ LV7Zadatak_1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV7Zadatak_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int unos;
+            Console.WriteLine("Izračunajte drugi korijen: ");
+
+            try
+            {
+                unos = int.Parse(Console.ReadLine());
+                double korijen = Math.Sqrt(unos);
+
+                if (unos <= 0)
+                {
+                    Console.WriteLine("\nPogrešan unos1!!");
+                }
+                else
+                {
+                    Console.WriteLine("\nDrugi korijen broja {0} iznosi: {1}", unos, korijen);
+                }
+            }
+            //} catch (ArithmeticException ex1)
+            //{
+            //    Console.WriteLine("\nPogrešan unos1!!");
+            //}
+            catch (FormatException ex2)
+            {
+                Console.WriteLine("\nPogrešan unos!!");
+            }
+            finally
+            {
+                Console.WriteLine("\nKraj programa!!");
+            }
+        }
+    }
+}
---
+++ LV7Zadatak_2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV7Zadatak_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int prviBroj;
+            int drugiBroj;
+            int treciBroj;
+            int zbroj;
+
+            try
+            {
+                Console.WriteLine("Unesite prvi broj: ");
+                prviBroj = int.Parse(Console.ReadLine());
+                Console.WriteLine("Unesite drugi broj: ");
+                drugiBroj = int.Parse(Console.ReadLine());
+                Console.WriteLine("Unesite treci broj: ");
+                treciBroj = int.Parse(Console.ReadLine());
+                zbroj = prviBroj + drugiBroj + treciBroj;
+
+                Console.WriteLine("\nZbroj unesenih brojeva iznosi: {0}", zbroj);
+            }
+            catch (FormatException ex1)
+            {
+                Console.WriteLine("\nNiste unijeli brojeve!!");
+            }
+            finally
+            {
+                Console.WriteLine("\nKraj programa!!");
+            }
+        }
+    }
+}
